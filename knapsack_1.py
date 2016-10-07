5e326af098982b524d2123e27a5b747d25e970e1
Bc. Miroslav Brabenec <brabemi3@fit.cvut.cz>
2016-10-07 21:13:06 +0200

    updated .gitignore added requirements.txt

--- knapsack_1.py
+++ knapsack_1.py
@@ -1,9 +1,29 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from random import randint
+
+# naivní opravdu všechny kombinace, rekurze
+# heuristika podle, ceny, váhy, cena/váha
+# naskládám předměty s nejvyšší cenou
+# naskládám předměty s nejnižsí vahou
+# naskládám předměty s nejlepším poměrěm vahou
+# cena, váha, cena/váha
+
+
+def generate_items(n, min_price, max_price, min_weight, max_weight):
+	retval = []
+	for i in range(n):
+		data = {'id':i+1, 'price': randint(min_price, max_price), 'weight': randint(min_weight, max_weight)}
+		data['ppw'] = data['price']/data['weight']
+		retval.append(data)
+	return retval
+
 def main(args):
-    return 0
+	items = generate_items(5, 1, 40, 1, 40)
+	print(items)
+	return 0
 
 if __name__ == '__main__':
-    import sys
-    sys.exit(main(sys.argv))
+	import sys
+	sys.exit(main(sys.argv))
